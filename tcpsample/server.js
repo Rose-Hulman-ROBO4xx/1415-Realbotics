67766bfa74a9dbae00922ff8b5dbfe17295a27a3
isabel <dillonscroggin@gmail.com>
2014-11-17 02:58:05 -0500

    Yay for function closures

--- tcpsample/server.js
+++ tcpsample/server.js
@@ -6,16 +6,16 @@ server.on('listening', function(){ console.log('Server running') });
 server.on('close', function(){ console.log('Server stopping') });
 
 server.on('connection', function(socket){
-	socket.name = '[' + socket.remoteAddress + ':' + socket.remotePort + ']';
-	console.log('Client connected from ' + socket.name);
+	var name = '[' + socket.remoteAddress + ':' + socket.remotePort + ']';
+	console.log('Client connected from ' + name);
 
 	socket.on('data', function(data){
-		console.log(socket.name + ': ' + data);
+		console.log(name + ': ' + data);
 		socket.write(data);
 	});
 
 	socket.on('close', function(error){
-		console.log('Client ' + socket.name + ' disconnected');
+		console.log('Client ' + name + ' disconnected');
 	});
 });
 
